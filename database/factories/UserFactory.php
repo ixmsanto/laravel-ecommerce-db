9be681edef4e28f1436ab2f2c3f8a7544423ef82
ixmsanto <santhoshjeje@gmail.com>
2025-05-02 01:13:04 +0530

    role added on def

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            // 'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
+            'role' => $this->faker->randomElement(['customer', 'admin']),
             'remember_token' => Str::random(10),
         ];
     }
